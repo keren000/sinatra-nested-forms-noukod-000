3eb64f67f3a3f3e3ecf3a6fc330cbbff58e1928c
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-11 17:33:43 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPES = []
+
+  def initialize(param)
+    @name = param[:name]
+    @type = param[:type]
+    @booty = param[:booty]
+    SHIPES << self
+  end
+
+  def self.all
+    SHIPES
+  end
+
+  def self.clear
+    SHIPES.clear
+  end
 end
